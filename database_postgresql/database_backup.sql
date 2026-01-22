257d3655ca3aa12d8198928bd544491c5db9796e
babu <babu.k@kavia.ai>
2026-01-22 13:39:12 +0000

    Auto-commit: session cgf0a7a460 - gourmet-delivery-platform-8388-8397

--- database_postgresql/database_backup.sql
+++ database_postgresql/database_backup.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database dump
 --
 
-\restrict jLwngufCIWqDYnCqeUIL0Xm0hGg2AbKzsMdY8MLdj8BTHPRILwMI3PeOcTurr9E
+\restrict vSoUTNnzEkWbcyiAGUQQADS98ezy9OcilPCrYlUlMQr8YeRfM8z4gVv1V9ElIRI
 
 -- Dumped from database version 16.11 (Ubuntu 16.11-0ubuntu0.24.04.1)
 -- Dumped by pg_dump version 16.11 (Ubuntu 16.11-0ubuntu0.24.04.1)
@@ -28,9 +28,9 @@ CREATE DATABASE myapp WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE_PROVIDE
 
 ALTER DATABASE myapp OWNER TO postgres;
 
-\unrestrict jLwngufCIWqDYnCqeUIL0Xm0hGg2AbKzsMdY8MLdj8BTHPRILwMI3PeOcTurr9E
+\unrestrict vSoUTNnzEkWbcyiAGUQQADS98ezy9OcilPCrYlUlMQr8YeRfM8z4gVv1V9ElIRI
 \connect myapp
-\restrict jLwngufCIWqDYnCqeUIL0Xm0hGg2AbKzsMdY8MLdj8BTHPRILwMI3PeOcTurr9E
+\restrict vSoUTNnzEkWbcyiAGUQQADS98ezy9OcilPCrYlUlMQr8YeRfM8z4gVv1V9ElIRI
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -89,5 +89,5 @@ ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA public GRANT ALL ON TABLES 
 -- PostgreSQL database dump complete
 --
 
-\unrestrict jLwngufCIWqDYnCqeUIL0Xm0hGg2AbKzsMdY8MLdj8BTHPRILwMI3PeOcTurr9E
+\unrestrict vSoUTNnzEkWbcyiAGUQQADS98ezy9OcilPCrYlUlMQr8YeRfM8z4gVv1V9ElIRI
 
